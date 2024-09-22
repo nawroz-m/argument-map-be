4b0e364d373569a72967f2627092100ae8f22511
nawroz-m <nawroz678@gmail.com>
2024-09-23 02:40:57 +0530

    update: dataset

--- api/controller/claimJsonController.js
+++ api/controller/claimJsonController.js
@@ -48,7 +48,7 @@ const data = {
       name: "John Doe",
       type: "request",
       claimId: "claim-02",
-      assignedObjection: [],
+      assignedObjection: ["objection-03"],
 
       evidence: [
         {
@@ -71,6 +71,87 @@ const data = {
           evidenceId: "evidence-2",
         },
       ],
+    },
+
+    {
+      title: "Unpaid Invoice",
+      complainant: "Acme Consulting LLC",
+      description:
+        "Client has not paid for services rendered as per contract dated 12/09/2024",
+      name: "John Doe",
+      type: "claim",
+      claimId: "claim-03",
+      assignedObjection: ["objection-04"],
+
+      evidence: [
+        {
+          title: "Invoice #12345",
+          description:
+            "Muhakemat nedir? Güvenli, Akıllı ve Entegre Dava Yönetim Sistemi Muhakemat platformu, Ünmaların dava süreçlerind yönetmelerini",
+          link: "https://example.com/invoice12345.pdf",
+          evidenceId: "evidence-04",
+        },
+      ],
+    },
+    {
+      title: "Insulted Invoice",
+      complainant: "Acme Consulting LLC",
+      description:
+        "Client has insulted for asking the services money rendered as per contract dated 12/09/2024",
+      name: "John Doe",
+      type: "claim",
+      claimId: "claim-04",
+      assignedObjection: ["objection-04"],
+
+      evidence: [
+        {
+          title: "Invoice #12345",
+          description:
+            "Muhakemat nedir? Güvenli, Akıllı ve Entegre Dava Yönetim Sistemi Muhakemat platformu, Ünmaların dava süreçlerind yönetmelerini",
+          link: "https://example.com/invoice12345.pdf",
+          evidenceId: "evidence-05",
+        },
+      ],
+    },
+    {
+      title: "Insulted Invoice",
+      complainant: "Acme Consulting LLC",
+      description:
+        "Client has insulted for asking the services money rendered as per contract dated 12/09/2024",
+      name: "John Doe",
+      type: "request",
+      claimId: "claim-04",
+      assignedObjection: ["objection-04"],
+
+      evidence: [
+        {
+          title: "Invoice #12345",
+          description:
+            "Muhakemat nedir? Güvenli, Akıllı ve Entegre Dava Yönetim Sistemi Muhakemat platformu, Ünmaların dava süreçlerind yönetmelerini",
+          link: "https://example.com/invoice12345.pdf",
+          evidenceId: "evidence-06",
+        },
+        {
+          title: "Invoice #12345",
+          description:
+            "Muhakemat nedir? Güvenli, Akıllı ve Entegre Dava Yönetim Sistemi Muhakemat platformu, Ünmaların dava süreçlerind yönetmelerini",
+          link: "https://example.com/invoice12345.pdf",
+          evidenceId: "evidence-07",
+        },
+      ],
+    },
+
+    {
+      title: "Insulted Invoice",
+      complainant: "Acme Consulting LLC",
+      description:
+        "Client has insulted for asking the services money rendered as per contract dated 12/09/2024",
+      name: "John Doe",
+      type: "request",
+      claimId: "claim-05",
+      assignedObjection: [],
+
+      evidence: [],
     },
   ],
   objection: [
@@ -124,12 +205,57 @@ const data = {
           link: "https://example.com/communication_log.docx",
           counterId: "counter-claim-02",
         },
+      ],
+    },
+    {
+      title: "Not valid for payment",
+      description: "Services were not rendered as specified in the contract",
+      respondent: "Acme Consulting LLC",
+      contactPerson: "John Doe",
+      type: "objective",
+      claimId: "claim-02",
+      ojbectionId: "objection-03",
+
+      counterClaim: [
         {
-          title: "Contract Clause",
+          title: "Service Quality Report",
           description:
-            "Highlighted contract clause regarding service quality guarantees",
-          link: "https://example.com/contract_clause.png",
-          counterId: "counter-claim-03",
+            "Muhakemat nedir? Güvenli, Akıllı ve Entegre Dava Yönetim Sistemi Muhakemat platformu, Ünmaların dava süreçlerind yönetmelerini",
+          link: "https://example.com/quality_report.pdf",
+          counterId: "counter-claim-04",
+        },
+        {
+          title: "Communication Log",
+          description:
+            "Muhakemat nedir? Güvenli, Akıllı ve Entegre Dava Yönetim Sistemi Muhakemat platformu, Ünmaların dava süreçlerind yönetmelerini Muhakemat nedir? Güvenli, Akıllı ve Entegre Dava Yönetim Sistemi Muhakemat platformu, Ünmaların dava süreçlerinde yönetmelerini Muhakemat platformu, Ün maların davaları süreçlerind yönetmelerini Muhakemat.",
+          link: "https://example.com/communication_log.docx",
+          counterId: "counter-claim-06",
+        },
+      ],
+    },
+    {
+      title: "Lied about thier services",
+      description: "Services were not rendered as specified in the contract",
+      respondent: "Acme Consulting LLC",
+      contactPerson: "John Doe",
+      type: "objective",
+      claimId: "claim-03",
+      ojbectionId: "objection-04",
+
+      counterClaim: [
+        {
+          title: "Service Quality Report",
+          description:
+            "Muhakemat nedir? Güvenli, Akıllı ve Entegre Dava Yönetim Sistemi Muhakemat platformu, Ünmaların dava süreçlerind yönetmelerini",
+          link: "https://example.com/quality_report.pdf",
+          counterId: "counter-claim-07",
+        },
+        {
+          title: "Communication Log",
+          description:
+            "Muhakemat nedir? Güvenli, Akıllı ve Entegre Dava Yönetim Sistemi Muhakemat platformu, Ünmaların dava süreçlerind yönetmelerini Muhakemat nedir? Güvenli, Akıllı ve Entegre Dava Yönetim Sistemi Muhakemat platformu, Ünmaların dava süreçlerinde yönetmelerini Muhakemat platformu, Ün maların davaları süreçlerind yönetmelerini Muhakemat.",
+          link: "https://example.com/communication_log.docx",
+          counterId: "counter-claim-08",
         },
       ],
     },
