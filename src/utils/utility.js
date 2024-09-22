822ac40ee19f820537d6fcc8679b392046f260e4
nawroz-m <nawroz678@gmail.com>
2024-09-22 14:14:58 +0530

    update: routes  json response

--- src/utils/utility.js
+++ src/utils/utility.js
@@ -1,9 +1,42 @@
+import HTTPStatus from "http-status";
+
+// Create a common success respnse for JSON
+const createSuccessResponseJson = (data) => {
+  const successRespons = {
+    success: true,
+    data: data,
+  };
+  return successRespons;
+};
+
+// JSON Response
+const sendJSONResponse = (res, statusCode, data) => {
+  res.status(statusCode).json(data);
+};
+
 // Success response
 export const successResponse = (res, data) => {
-  return res;
+  const code = HTTPStatus.OK;
+  const response = createSuccessResponseJson(data);
+  return sendJSONResponse(res, code, response);
+};
+
+//Error Response
+const createErrorResponseJSON = (error) => {
+  const errorResponse = {
+    error: error,
+  };
+  return errorResponse;
 };
 
 // Success response
 export const badRequestResponse = (res, error) => {
-  return res;
+  const data = {
+    message: error,
+  };
+  code = HTTPStatus.BAD_REQUEST;
+
+  response = createErrorResponseJSON(data);
+
+  return sendJSONResponse(res, code, response);
 };
